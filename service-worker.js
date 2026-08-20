c530088694f7693d234e5b5aa2b1cc4e669d8f08
msmith0514 <matthew.a.smith0514@gmail.com>
2026-08-20 10:52:30 -0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(key => caches.delete(key)));
+    await self.registration.unregister();
+    const clientsList = await self.clients.matchAll({ type: 'window', includeUncontrolled: true });
+    for (const client of clientsList) {
+      client.navigate(client.url);
+    }
+  })());
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
